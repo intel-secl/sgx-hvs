f0028e5f291c71609bdaa68a6434fd6ff619251b
Raviraj Bhat <raviraj.bhat@intel.com>
2020-08-21 06:35:09 -0700

    remove unused Mutex var

--- config/config.go
+++ config/config.go
@@ -15,7 +15,6 @@ import (
 	"os"
 	"path"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -65,8 +64,6 @@ type Configuration struct {
 	IdleTimeout       time.Duration
 	MaxHeaderBytes    int
 }
-
-var mu sync.Mutex
 
 var global *Configuration
 
