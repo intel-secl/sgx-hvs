d6f0a7ebc1e1798aa76d054917e384897a67e7fa
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-04-27 03:48:16 -0700

    ISECL-15473: restore config file if any of setup task fails in container deployment

--- dist/image/entrypoint.sh
+++ dist/image/entrypoint.sh
@@ -32,20 +32,24 @@ if [ ! -f $CONFIG_PATH/.setup_done ]; then
 fi
 
 if [ ! -z "$SETUP_TASK" ]; then
+  cp $CONFIG_PATH/config.yml /tmp/config.yml
   IFS=',' read -ra ADDR <<<"$SETUP_TASK"
   for task in "${ADDR[@]}"; do
     if [[ "$task" == "update_service_config" || "$task" == "database" || "$task" == "all" ]]; then
       shvs setup $task
       if [ $? -ne 0 ]; then
+        cp /tmp/config.yml $CONFIG_PATH/config.yml
         exit 1
       fi
       continue 1
     fi
     shvs setup $task --force
     if [ $? -ne 0 ]; then
+      cp /tmp/config.yml $CONFIG_PATH/config.yml
       exit 1
     fi
   done
+  rm -rf /tmp/config.yml
 fi
 
 shvs run
